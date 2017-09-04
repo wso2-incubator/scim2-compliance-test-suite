76876a6049628b6c147487e35db03fc60f416036
Vindula Jayawardana <vindulamj@gmail.com>
2017-09-04 15:56:26 +0530

    Trust all certificates using HttpClient over HTTPS

--- src/main/java/info/wso2/scim2/compliance/httpclient/HTTPClient.java
+++ src/main/java/info/wso2/scim2/compliance/httpclient/HTTPClient.java
@@ -15,25 +15,54 @@
  */
 package info.wso2.scim2.compliance.httpclient;
 
+import info.wso2.scim2.compliance.exception.ComplianceException;
 import info.wso2.scim2.compliance.protocol.ComplianceTestMetaDataHolder;
 import org.apache.commons.codec.binary.Base64;
 import org.apache.http.HttpHeaders;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpRequestBase;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.ssl.SSLContextBuilder;
+import org.apache.http.ssl.TrustStrategy;
 
 import java.nio.charset.Charset;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 
 /**
  * This class is to depicts the HTTP client.
  */
 public class HTTPClient {
 
-    private static HttpClient httpClient = null;
+    private static CloseableHttpClient httpClient = null;
 
-    public static HttpClient getHttpClient() {
+    public static HttpClient getHttpClient() throws ComplianceException {
         if(httpClient == null) {
-            HttpClient httpClient = HttpClientBuilder.create().disableAutomaticRetries().build();
+            TrustStrategy trustAllStrategy = new TrustStrategy(){
+                public boolean isTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+                    return true;
+                }
+            };
+            SSLContextBuilder builder = new SSLContextBuilder();
+            try {
+                builder.loadTrustMaterial(trustAllStrategy);
+            } catch (NoSuchAlgorithmException | KeyStoreException e) {
+                throw new ComplianceException("Error in setting up the http client");
+            }
+
+            SSLConnectionSocketFactory sslsf = null;
+            try {
+                sslsf = new SSLConnectionSocketFactory(builder.build());
+            } catch (NoSuchAlgorithmException | KeyManagementException e){
+                throw new ComplianceException("Error in setting up the http client");
+            }
+            httpClient = HttpClients.custom().setSSLSocketFactory(sslsf).build();
             return httpClient;
         }
         return httpClient;
